b5706f3639d54125480305da0ced142e91d578a9
Palmiére <palmiere.santana@escolar.ifrn.edu.br>
2024-12-09 21:17:31 -0300

    criação da área de usuário ADMIN para cadastro de novos usuários.

--- memoria/urls.py
+++ memoria/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.cadastrar, name='cadastrar'),
+    path('login/', views.login, name='login'),
+    path('index/', views.index, name='index'),
     path('jogo/', views.jogo, name='jogo'),
     path('ranking/', views.ranking, name='ranking'),
 ]
--- memoria/views.py
+++ memoria/views.py
@@ -1,13 +1,59 @@
 from datetime import timezone
 from django.shortcuts import redirect, render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Jogo
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+def cadastrar(request):
+    if request.method == "GET":
+        return render(request, 'memoria/cadastrar.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+    
+        user = User.objects.filter(username=username).first()
+        
+        if user:
+            return HttpResponse('Já possui um usuario logado com esse username')
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return redirect('login')
+     
+
+    
+def login(request):
+    if request.method == "GET":
+        return render(request, 'memoria/login.html')
+    
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = authenticate(username=username, password=senha)
+        
+        if user:
+            return redirect('index')
+        else:
+            return HttpResponse('email ou senha invalidos')
+        
+def plataforma (request):
+    if request.user.is_authenticated:
+     return HttpResponse('Plataforma')
+ 
+    else:
+        return HttpResponse('Você precisa está logado para acessar essa área')
+    
+@login_required(login_url='login')
 def index(request):
     return render(request, 'memoria/index.html')
 
+@login_required(login_url='login')
 @csrf_exempt 
 def jogo(request):
     if request.method == 'POST':
